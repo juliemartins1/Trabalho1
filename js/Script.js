cda6d7253151cb9a7ccacc8f40b16d66a0631594
juliemartins1 <166827680+juliemartins1@users.noreply.github.com>
2025-04-06 20:32:54 -0300

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -210,9 +210,20 @@ a nova largura de borda.*/
             });
             
             // Adiciona os elementos ao DOM
+            
             itemControl.appendChild(itemInput);
+            /*adiciona o campo de texto (itemInput) ao container de controle do item (itemControl). O itemInput é um elemento <input> que
+            permite ao usuário editar o texto de um item do menu. Usando o método appendChild(), 
+            este campo de texto se torna um elemento filho dentro do container.*/
             itemControl.appendChild(removeButton);
+            /*itemControl.appendChild(removeButton);
+Esta linha adiciona o botão de remoção (removeButton) ao mesmo container de controle (itemControl), colocando-o logo após o campo de texto. 
+Este botão tem o símbolo "×" e permite ao usuário excluir o item específico do menu. 
+Ele também é adicionado como um elemento filho do container de controle.*/
             menuItemsContainer.appendChild(itemControl);
+            /*Esta última linha adiciona todo o conjunto de controle de item (que agora contém tanto o campo de texto quanto o botão de remoção) ao 
+            container principal de itens do menu (menuItemsContainer). 
+            O menuItemsContainer é o elemento na interface que contém todos os controles de itens do menu.*/
         });
     }
     
